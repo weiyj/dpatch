c1fd98d381b73ddf8b1e0bff4679195d90c83bc9
Wei Yongjun <weiyj.lk@gmail.com>
2012-11-01 15:55:46 +0800

    crond: change the time to 14pm
    Signed-off-by: Wei Yongjun <weiyj.lk@gmail.com>

--- config/dpatch.sh
+++ config/dpatch.sh
@@ -1,2 +1,2 @@
-# Run dpatch dailyupdate once a day at 11am by default
-0 11 * * * dpatch /usr/share/dpatch/bin/dailypatch.sh
+# Run dpatch dailyupdate once a day at 14pm by default
+0 14 * * * dpatch /usr/share/dpatch/bin/dailypatch.sh
